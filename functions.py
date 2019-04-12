9072fc730adbd7fee6ae392aa5f74eebfd530010
Cameron Hutchings <camhutch12@gmail.com>
2019-04-11 22:21:31 -0400

    the next part of the tutorial

--- functions.py
+++ functions.py
@@ -5,16 +5,23 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def center_text(text):
-    text = str(text)
+'''* allows for multiple variables 
+we also declare default parameters in python '''
+
+
+def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80 - len(text) // 2)
-    print(" " * left_margin, text)
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
 
+'''writes the current information to a file'''
+with open("centered",mode='w') as center_file:
+    python_food()
+    center_text("spam spam spam and spam",file=center_file)
+    center_text(12)
+    center_text("spam spam and eggs",file=center_file)
 
-python_food()
-center_text("spam spam spam and spam")
-center_text(12)
-center_text("spam spam and eggs")
-
-print("First","second","3","4",'spam')
-
+    center_text("First", "second", "3", "4", 'spam', sep="-",file=center_file)
+    print()
